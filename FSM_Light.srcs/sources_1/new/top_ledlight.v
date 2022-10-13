8dd905d38a611793d9aadf0b1e7c90c954b41923
ekgh2002 <ekgh2002@naver.com>
2022-10-13 18:56:09 +0900

    first commit

---
+++ FSM_Light.srcs/sources_1/new/top_ledlight.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+
+module top_ledlight(
+    input i_clk,
+    input i_reset,
+    input [1:0] i_button,
+    output [1:0] o_light
+    );
+
+    wire [1:0] w_button;
+    Button_Controller bt_control0(
+    .i_clk(i_clk),
+    .i_button(i_button[0]),
+    .i_reset(i_reset),
+    .o_button(w_button[0])       
+    );
+
+
+    Button_Controller bt_control1(
+    .i_clk(i_clk),
+    .i_button(i_button[1]),
+    .i_reset(i_reset),
+    .o_button(w_button[1])       
+    );
+
+    FSM_Light light(
+    .i_clk(i_clk),
+    .i_reset(i_reset),
+    .i_button(w_button),
+    .o_light(o_light)
+    );
+
+endmodule
